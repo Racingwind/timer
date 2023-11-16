6c6b1d4ae69c7970a96e147b46daa4daf2b7f895
Racingwind <louis.k.hu@gmail.com>
2023-11-16 02:19:17 -0800

    implemented playing system sound

---
+++ timer1.js
@@ -0,0 +1,20 @@
+// No numbers are provided: Easy. It should just not beep at all and end immediately since no beeps should get scheduled.
+// An input is a negative number: Ignore/skip any numbers that are negative. We can't schedule anything in the past.
+// An input is not a number: Ignore/skip these as well, instead of attempting to call setTimeout with a non-number.
+
+let inputs = process.argv.slice(2);
+
+if (inputs === []) { // if empty, return immediately
+  return;
+};
+
+inputs = inputs.map(x => Number(x)); // convert into numbers
+
+for (let entry of inputs) {
+  if (typeof entry === "number" && entry >= 0) {
+    setTimeout(() => {
+      console.log(entry); // adding a write console here as my system sound takes longer than 1 second to play, so consecutive integers will not play the second sound
+      process.stdout.write('\x07');
+    }, (entry * 1000));
+  }
+}
